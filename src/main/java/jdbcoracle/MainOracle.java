54332e7c41e9b29b423582aecf5c6769d0838c11
unceasingwaves92 <73957442+unceasingwaves92@users.noreply.github.com>
2022-10-18 21:52:22 +0530

    Update MainOracle.java

--- src/main/java/jdbcoracle/MainOracle.java
+++ src/main/java/jdbcoracle/MainOracle.java
@@ -7,7 +7,7 @@ public class MainOracle {
     public static void main(String[] args) throws IOException {
        // Static method no need to create object method
         DBConnection.getOracleDB("C:\\Users\\Dell\\IdeaProjects\\OracleDB\\src\\resources\\db.properties");
-        DBConnection.getConnectionFirstMethod("jdbc:oracle:thin:@localhost:1521:xe", "system", "******");
-        DBConnection.getConnectionSecondMethod("jdbc:oracle:thin:@localhost:1521:xe?user=system&password=******");
+        DBConnection.getConnectionFirstMethod("jdbc:oracle:thin:@localhost:1521:xe", "system", "nedumaran@86");
+        DBConnection.getConnectionSecondMethod("jdbc:oracle:thin:@localhost:1521:xe?user=system&password=nedumaran@86");
     }
 }
