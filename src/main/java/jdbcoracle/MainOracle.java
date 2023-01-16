259719416fed30c109b03785c2a8069c4ba4c032
karthikeyan <unceasingwaves92@gmail.com>
2023-01-16 22:43:13 +0530

    updated code

--- src/main/java/jdbcoracle/MainOracle.java
+++ src/main/java/jdbcoracle/MainOracle.java
@@ -21,18 +21,17 @@ public class MainOracle {
             String response = userDAO.saveUser(user);
             System.out.println(response);
 
-            user.setDescription("welcome java");
-            user.setApplication_name("database");
-            user.setOwner("manika");
-            user.setApplication_id(3);
-            System.out.println(userDAO.updateContact(user));
-            System.out.println(userDAO.getAllUsers());
+            //user.setDescription("welcome java");
+            //user.setApplication_name("database");
+            //user.setOwner("manika");
+            //user.setApplication_id(3);
+          //  System.out.println(userDAO.updateContact(user));
+          //  System.out.println(userDAO.getAllUsers());
             System.out.println(userDAO.selectContact("3"));
             userDAO.deleteContact(3);
         } catch (SQLException  e) {
             e.printStackTrace();
-        }
-    }
+        }    }
 }
 
 
